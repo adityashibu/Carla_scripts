7503dd0578e4484bb62e0e2d290819c8f3912f1f
Aditya S <as2397@hw.ac.uk>
2025-02-21 18:11:20 +0400

    Refactor camera setup and vehicle spawning; add RGB sensor and output directory creation

--- camera.py
+++ camera.py
@@ -1,31 +1,42 @@
 import carla
-import math
-import random 
-import time 
+import time
+import os
+import cv2
 import numpy as np
-# import cv2
 
-client = carla.Client('localhost', 2000)
+client = carla.Client("localhost", 2000)
+client.set_timeout(10.0)
 world = client.get_world()
 
-bp_lib = world.get_blueprint_library()
-spawn_points = world.get_map().get_spawn_points()
+bp_library = world.get_blueprint_library()
+vehicle_bp = bp_library.find('vehicle.dodge.charger')
 
-# for bp in bp_lib.filter('vehicle'):
-#     print(bp.id)
+transform = world.get_map().get_spawn_points()[75]
+vehicle = world.spawn_actor(vehicle_bp, transform)
 
-vehicle_bp = bp_lib.find('vehicle.dodge.charger')
-vehicle = world.spawn_actor(vehicle_bp, random.choice(spawn_points))
+def camFunc(data):
+    i = np.array(data.raw_data)
+    i2 = i.reshape((720, 1024, 4))
+    i3 = i2[:, :, :3]
+    
+    cv2.imshow("", i3)
+    cv2.waitKey(1)
 
+rgb_cam = bp_library.find('sensor.camera.rgb')
+rgb_cam.set_attribute("image_size_x", "1024")
+rgb_cam.set_attribute("image_size_y", "720")
+rgb_cam.set_attribute("fov", "90")
 
-spectator = world.get_spectator()
-transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4, z=2.5)), vehicle.get_transform().rotation)
-spectator.set_transform(transform)
+transform = carla.Transform(carla.Location(x=4,z=1.6),carla.Rotation(roll=0,pitch=0,yaw=0))
+sensor = world.spawn_actor(rgb_cam, transform, attach_to=vehicle)
 
-camera_bp = bp_lib.find('sensor.camera.rgb')
-camera_init_trans = carla.Transform(carla.Location(z=1, x=0.6))
-camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
+if not os.path.exists("output\\"):
+    os.mkdir("output\\")
+#sensor.listen(lambda data: data.save_to_disk('output/%.6d.jpg' % data.frame))
+sensor.listen(lambda data: camFunc(data))
 
-time.sleep(0.2)
-spectator.set_transform(camera.get_transform())
-camera.destroy()
+vehicle.set_autopilot(True)
+
+time.sleep(60)
+sensor.destroy()
+vehicle.destroy()
